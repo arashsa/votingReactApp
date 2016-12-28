21d797553c86e40530ef95f73dbb54bcb7aa973e
Arash Saidi <garash84@gmail.com>
2016-12-28 13:44:19 +0100

    test

---
+++ client/main.js
@@ -0,0 +1,3 @@
+import Test from './test.js'
+
+a = new Test();
---
+++ client/test.js
@@ -0,0 +1,8 @@
+class Test {
+    constructor() {
+        console.log('created Test class');
+        
+    }
+}
+
+export default Test
