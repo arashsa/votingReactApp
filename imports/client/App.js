302ff206a167371e882199184895578b71ca3bc9
Arash Saidi <garash84@gmail.com>
2016-12-28 22:26:10 +0100

    Added an internal function to the class

--- imports/client/App.js
+++ imports/client/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+let hello = 0;
+
 export default class App extends Component {
+    addOne() {
+        console.log(hello)
+        hello += 1;
+    }
     render() {
         return (
-            <h1>Hello world!</h1>
+            <h1 onClick={this.addOne}>Hello! Number: {hello} </h1>
         );
     }
 }
