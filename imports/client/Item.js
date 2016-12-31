658fa2d161d228846629a235af347aac9cd7591c
Arash Saidi <garash84@gmail.com>
2016-12-31 15:41:35 +0100

    Added user checks

--- imports/client/Item.js
+++ imports/client/Item.js
@@ -3,30 +3,39 @@ import Items from '../api/Items';
 
 export default class Item extends Component {
     voteOne() {
-        Items.update({_id: this.props.item._id}, {$inc:{
-            'itemOne.value': 1 
-        }});
+        if (Meteor.userId()) {
+            Items.update({ _id: this.props.item._id }, {
+                $inc: {
+                    'itemOne.value': 1
+                }
+            });
+        }
     }
 
     voteTwo() {
-        Items.update({_id: this.props.item._id}, {$inc:{
-            'itemTwo.value': 1 
-        }});
+        if (Meteor.userdID()) {
+            Items.update({ _id: this.props.item._id }, {
+                $inc: {
+                    'itemTwo.value': 1
+                }
+            });
+
+        }
     }
 
     render() {
         return (
             <div className='item'>
                 <div className='vote-one noselect' onClick={this.voteOne.bind(this)}>
-                <span>{this.props.item.itemOne.value}</span>
-                <h3>{this.props.item.itemOne.text}</h3>
+                    <span>{this.props.item.itemOne.value}</span>
+                    <h3>{this.props.item.itemOne.text}</h3>
                 </div>
 
                 <span>vs</span>
 
                 <div className='vote-two noselect' onClick={this.voteTwo.bind(this)}>
-                <span>{this.props.item.itemTwo.value}</span>
-                <h3>{this.props.item.itemTwo.text}</h3>
+                    <span>{this.props.item.itemTwo.value}</span>
+                    <h3>{this.props.item.itemTwo.text}</h3>
                 </div>
             </div>
         )
